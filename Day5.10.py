d52f9c85b8bb6348eb6cd9ac2ac9d0664aaedcf9
SESHA SAI BHATTIPROLU <112627809+SESHASAIBHATTIPROLU@users.noreply.github.com>
2022-09-23 10:44:25 +0530

    Add files via upload

---
+++ Day5.10.py
@@ -0,0 +1,10 @@
+# input string
+string = "Red rose"
+# reversing words in a given string
+s = string.split()[::-1]
+l = []
+for i in s:
+	# apending reversed words to l
+	l.append(i)
+# printing reverse words
+print(" ".join(l))
